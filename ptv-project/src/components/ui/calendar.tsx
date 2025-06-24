b52b7e7fc36f9f67f16848835863d4c60a84650a
AdlayLevy <al@setpay.mx>
2025-06-24 13:39:00 -0600

    minor errors

--- ptv-project/src/components/ui/calendar.tsx
+++ ptv-project/src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
